b7b245d8241c49133c1a56b058a0e758a66d4f50
bhushan2311 <bambhore350@gmail.com>
2023-02-21 02:30:54 +0530

    Added CSS

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
+import "./index.css";
 
 const ImageCaptionGenerator = () => {
   const [image, setImage] = useState("");
   const [preview, setPreview] = useState("");
-  const [caption, setCaption] = useState("");
+  const [caption, setCaption] = useState("This is dummy caption");
 
-  
+
   const handleImageChange = (event) => {
     const img = event.target.files[0];
     setImage(img);
@@ -17,14 +18,24 @@ const ImageCaptionGenerator = () => {
   };
 
   return (
-    <div>
-      <input type="file" onChange={handleImageChange} />
-      {preview && <img src={preview} alt="image" />}
-      <button onClick={handleGenerateCaption}>Generate Caption</button>
-      <p>{caption}</p>
-    </div>
+    <>
+      <div className="divtop">
+        <div className="div1">
+          <h1 className="heading">Image Caption Generator</h1>
+          <input type="file" onChange={handleImageChange} />
+          <div className="imgdiv">
+            {preview && <img className="imgcss" src={preview} alt="image" />}
+          </div>
+          <p style={{fontWeight:"bold"}}>{caption}</p>
+          <div>
+            <button className="btn" onClick={handleGenerateCaption}>Generate Caption</button>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
 export default ImageCaptionGenerator;
+
 
